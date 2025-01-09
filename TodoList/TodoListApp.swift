5e31ef95fe2321d246488f0044524fd266207e6f
Hasan Masrur <mhbabu99@gmail.com>
2025-01-09 15:07:09 +0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Hasan Masrur on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListTests/TodoListTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodoListTests.swift
+//  TodoListTests
+//
+//  Created by Hasan Masrur on 9/1/25.
+//
+
+import Testing
+
+struct TodoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
